390c261c14ae564dbc233265329d79cda221ab15
Noah Hellman <noah.hellman@protonmail.com>
2019-12-29 13:22:23 +0200

    apply stronger morphological opening

--- src/preprocess/segment_coins.m
+++ src/preprocess/segment_coins.m
@@ -5,12 +5,12 @@ function [centers, radii] = segment_coins(I, checkerboard_points, board_size)
     T = graythresh(Iv_covered);
 
     I_otsu = ~imbinarize(Iv, T);
-    I_otsu = imopen(imclose(I_otsu, ones(5)), ones(3));
+    I_otsu = imopen(imclose(I_otsu, ones(10)), ones(3));
 
     [centers, radii, metric] = imfindcircles(I_otsu, [10 size(Iv, 1)], ...
                                              'ObjectPolarity', 'bright', ...
                                              'Sensitivity', 0.9);
-    METRIC_THRESHOLD = 0.2;
+    METRIC_THRESHOLD = 0.15;
     centers = centers(metric > METRIC_THRESHOLD, :);
     radii = radii(metric > METRIC_THRESHOLD, :);
 end
